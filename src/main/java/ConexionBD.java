506ec30f10517a12e2b1c00440ae71701a7a7e34
miguelararat <miguel.ararat@uao.edu.co>
2026-09-24 20:41:16 -0500

    Commit de prueba

--- src/main/java/ConexionBD.java
+++ src/main/java/ConexionBD.java
@@ -6,7 +6,7 @@ public class ConexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/taller_mecanico";
     private static final String USER = "root";
     private static final String PASSWORD = "";
-    private static final string;
+     private static final String hola = "";
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
